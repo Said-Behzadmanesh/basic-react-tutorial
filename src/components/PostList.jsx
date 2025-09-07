c6f2d8cdca9800a03fbfad75cf8b5e8500f943b1
Said Behzadmanesh <behmanesh_s@yahoo.com>
2025-09-07 12:24:36 +0200

    handle backend delays

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,12 +6,15 @@ import { useEffect, useState } from "react";
 
 export default function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const data = await response.json();
       setPosts(data.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -36,17 +39,22 @@ export default function PostList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>No posts yet!</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>Loading posts...</h2>
         </div>
       )}
     </>
